15cd975c47d8df7370352117172aed4793a87c27
David Rodriguez <david@bsdadm.com>
2021-04-05 11:23:37 -0400

    refactor pages and add themeprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import About from './pages/About';
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
+import Contact from './pages/Contact/Contact';
+import About from './pages/About/About';
 import { Route, Switch } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core';
+import { CssBaseline, Container } from '@material-ui/core';
 import Nav from './components/Navigation/Nav';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: 'Montserrat',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  },
+});
+
 function App() {
   return (
     <>
-      <CssBaseline />
-      <Nav />
-
-      <Switch>
-        <Route exact from='/' render={(props) => <Home {...props} />} />
-        <Route
-          exact
-          from='/projects'
-          render={(props) => <Projects {...props} />}
-        />
-        <Route
-          exact
-          from='/contact'
-          render={(props) => <Contact {...props} />}
-        />
-        <Route exact from='/about' render={(props) => <About {...props} />} />
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Nav />
+        <Container>
+          <Switch>
+            <Route exact from='/' render={(props) => <Home {...props} />} />
+            <Route
+              exact
+              from='/projects'
+              render={(props) => <Projects {...props} />}
+            />
+            <Route
+              exact
+              from='/contact'
+              render={(props) => <Contact {...props} />}
+            />
+            <Route
+              exact
+              from='/about'
+              render={(props) => <About {...props} />}
+            />
+          </Switch>
+        </Container>
+      </ThemeProvider>
     </>
   );
 }
