a3ae716de9b9cafeac1d83a65805f175f49b2407
David Rodriguez <david@bsdadm.com>
2021-04-06 17:37:04 -0400

    refact custom project comp for Masonry grid

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import ProjectCard from '../../components/ProjectCard/ProjectCard';
+import Masonry from 'react-masonry-css';
+import projects from '../../config/projects';
+import useStyles from './styles';
 
 const Projects = () => {
+  const classes = useStyles();
+  const breakpoints = {
+    default: 3,
+    1100: 2,
+    700: 1,
+  };
   return (
-    <>
-      <h2>This is the projects page</h2>
-    </>
+    <Masonry
+      breakpointCols={breakpoints}
+      className={classes.myMasonryGrid}
+      columnClassName={classes.myMasonryGridColumn}>
+      {projects.map(
+        ({ title, subheader, image, imgHeight, id, description }) => (
+          <div className={classes.myMasonryGridColumnDiv} key={id}>
+            <ProjectCard
+              title={title}
+              subheader={subheader}
+              image={image}
+              imgHeight={imgHeight}
+              description={description}
+            />
+          </div>
+        )
+      )}
+    </Masonry>
   );
 };
 
