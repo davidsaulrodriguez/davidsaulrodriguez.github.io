3892a8dcdb385a7f2d72f94bb5e7e627f6e7b7c4
David Rodriguez <david@bsdadm.com>
2021-04-11 12:13:53 -0400

    add homepage content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import useStyles from './styles';
+import { Typography } from '@material-ui/core';
 
 const Home = () => {
+  const classes = useStyles();
   return (
-    <>
-      <h2>This is the Homepage</h2>
-    </>
+    <div className={classes.root}>
+      {/* <div className={classes.contentContainer}> */}
+      <Typography variant='h2'>David Rodriguez</Typography>
+
+      <Typography variant='h5'>Full Stack Web Developer</Typography>
+      {/* </div> */}
+    </div>
   );
 };
 
