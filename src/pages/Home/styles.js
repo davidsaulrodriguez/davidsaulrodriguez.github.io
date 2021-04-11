742431e262cc15abcdb3552b1405cff8f65ff551
David Rodriguez <david@bsdadm.com>
2021-04-11 12:14:11 -0400

    add homepage styles

---
+++ src/pages/Home/styles.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '80vh',
+    flexDirection: 'column',
+  },
+}));
+
+export default useStyles;
