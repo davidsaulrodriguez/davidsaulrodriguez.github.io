9054bd434a7454356e7656954319aeeb83919483
David Rodriguez <david@bsdadm.com>
2021-04-11 20:41:04 -0400

    fix unwanted padding on footer

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -2,7 +2,10 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    padding: theme.spacing(3),
+    padding: theme.spacing(0, 3),
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
     textAlign: 'center',
     marginTop: 'auto',
     color: 'white',
